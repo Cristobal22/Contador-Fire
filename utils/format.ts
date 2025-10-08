63b29e2880071f67c87ab08dc482e0b0d66608ce
Cristobal22 <obaltoqui@gmail.com>
2025-10-08 21:27:35 +0000

    fix(format): Exporta la función validateRut que faltaba

--- utils/format.ts
+++ utils/format.ts
@@ -12,3 +12,20 @@ export const unformatRut = (rut: string): string => {
     if (!rut) return '';
     return rut.replace(/\./g, '').replace('-', '');
 };
+
+const getRutVerifier = (body: string): string => {
+    let sum = 0;
+    let M = 0, T = 1;
+    for (; body; body = Math.floor(body / 10).toString()) {
+        T = (T + body.slice(-1) * (9 - M++ % 6)) % 11;
+    }
+    return T ? (T - 1).toString() : 'K';
+};
+
+export const validateRut = (rut: string): boolean => {
+    if (!rut) return false;
+    const cleanRut = unformatRut(rut);
+    const body = cleanRut.slice(0, -1);
+    const verifier = cleanRut.slice(-1).toUpperCase();
+    return getRutVerifier(body) === verifier;
+};
