8c5fec5eafc9dbbfdb78f8b72a1706719f034002
Cristobal22 <obaltoqui@gmail.com>
2025-10-09 21:21:05 +0000

    fix: Corregir ruta de navegación para la vista de administración de usuarios

--- navigation.admin.ts
+++ navigation.admin.ts
@@ -3,12 +3,17 @@ import type { NavStructure } from './types';
 
 export const adminNavStructure: NavStructure = {
     'Dashboard': { icon: 'dashboard', path: '/dashboard', children: {} },
+    'Administración': {
+        icon: 'admin_panel_settings',
+        children: {
+            'Usuarios': { path: '/admin/users' },
+        }
+    },
     'Configuración': {
         icon: 'settings',
         children: {
             'General': { icon: 'tune', children: {
                 'Empresas': { path: '/configuracion/general/empresas' },
-                'Usuarios': { path: '/configuracion/general/usuarios' },
                 'Parámetros Mensuales': { path: '/configuracion/general/parametros-mensuales' }
             }}
         }
